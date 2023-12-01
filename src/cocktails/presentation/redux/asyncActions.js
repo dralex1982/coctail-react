d9edf82d60d0000b218dd4f95a03987065cd5e4f
Алексей Журавлев <dralex1982@gmail.com>
2023-12-01 11:10:42 +0200

    Refactor to Redux

--- src/cocktails/presentation/redux/asyncActions.js
+++ src/cocktails/presentation/redux/asyncActions.js
@@ -3,7 +3,7 @@ import {setCategories, setRandomCocktail} from "./actions";
 import {getRandomCocktail} from "../../data/fake/fakeApi";
 import {Cocktail} from "../../domain/model/Cocktail";
 import convertCategoryArray from "../../domain/use_cases/convertCategoryArray";
-import {getCategories} from "../../data/server/api";
+import {getCategories} from "../../data/fake/fakeApi";
 
 export const getRandomCocktailAction = (dispatch) =>{
     const newCocktail = getRandomCocktail();
